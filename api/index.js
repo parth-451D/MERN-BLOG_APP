09fd62c0e8f97350099f9cf1f1455f925de6575d
parth-451D <parth.dalsaniya2001@gmail.com>
2021-07-23 15:40:54 +0530

    final commit

--- api/index.js
+++ api/index.js
@@ -7,14 +7,18 @@ const userRoute = require('./routes/users');
 const postRoute = require('./routes/posts');
 const categoryRoute = require('./routes/categories');
 const multer = require("multer");
+const path = require("path");
 
 dotenv.config(); 
 app.use(express.json());
+app.use("/images", express.static(path.join(__dirname,"/images")));
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
-    useCreateIndex:true})
+    useCreateIndex:true,
+    useFindAndModify:true
+  })
     .then(console.log("Connected to MongoDB")).catch(err=>console.log(err));
 
     const storage = multer.diskStorage({
@@ -22,9 +26,9 @@ mongoose.connect(process.env.MONGO_URL, {
           cb(null, "images");
         },
         filename: (req, file, cb) => {
-          cb(null, "myimage.png");
+          cb(null, req.body.name);
         },
-      });
+      }); 
       
       const upload = multer({ storage: storage });
       app.post("/api/upload", upload.single("file"), (req, res) => {
